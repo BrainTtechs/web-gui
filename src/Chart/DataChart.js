ce4e9eb8eb3e7928847e6227e1b0b7781915e121
fatihkaan22 <fatihkaan22@gmail.com>
2022-05-04 18:36:42 +0300

    fix: data channels

--- src/Chart/DataChart.js
+++ src/Chart/DataChart.js
@@ -2,16 +2,15 @@ import MultiChannelChart from './index';
 import { styler } from 'react-timeseries-charts';
 
 const DataChart = ({ data }) => {
-
   const style = styler([
-    { key: 'p1_740', color: '#d32f2f' },
-    { key: 'p2_740', color: '#1976d2' },
-    { key: 'p3_740', color: 'green' },
-    { key: 'p4_740', color: '#9c27b0' },
-    { key: 'p1_850', color: '#ed6c02' },
-    { key: 'p2_850', color: '#0288d1' },
-    { key: 'p3_850', color: '#2e7d32' },
-    { key: 'p4_850', color: 'steelblue' }
+    { key: 'p1_740', color: '#d32f2f', width: 4 },
+    { key: 'p2_740', color: '#1976d2', width: 4 },
+    { key: 'p3_740', color: 'green', width: 4 },
+    { key: 'p4_740', color: '#9c27b0', width: 4 },
+    { key: 'p1_850', color: '#ed6c02', width: 4 },
+    { key: 'p2_850', color: '#0288d1', width: 4 },
+    { key: 'p3_850', color: '#2e7d32', width: 4 },
+    { key: 'p4_850', color: 'steelblue', width: 4 }
   ]);
 
   const baselineStyles = {
@@ -19,7 +18,7 @@ const DataChart = ({ data }) => {
       stroke: 'steelblue',
       opacity: 0.5,
       width: 0.25
-    },
+    }
   };
 
   const channels = {
@@ -101,16 +100,16 @@ const DataChart = ({ data }) => {
     for (let i = 0; i < data.time.length; i += 1) {
       if (i > 0) {
         // const time = data.time[i] * 1000;
-        const time = data.time[i] * 400;
+        const time = data.time[i] * 580;
 
         points['p1_740'].push([time, data.p1_740[i]]);
         points['p2_740'].push([time, data.p2_740[i]]);
         points['p3_740'].push([time, data.p3_740[i]]);
         points['p4_740'].push([time, data.p4_740[i]]);
-        points['p1_850'].push([time, data.p1_850[i]]);
-        points['p2_850'].push([time, data.p2_850[i]]);
-        points['p3_850'].push([time, data.p3_850[i]]);
-        points['p4_850'].push([time, data.p4_850[i]]);
+        // points['p1_850'].push([time, data.p1_850[i]]);
+        // points['p2_850'].push([time, data.p2_850[i]]);
+        // points['p3_850'].push([time, data.p3_850[i]]);
+        // points['p4_850'].push([time, data.p4_850[i]]);
       }
     }
 
@@ -128,11 +127,11 @@ const DataChart = ({ data }) => {
         'p1_740',
         'p2_740',
         'p3_740',
-        'p4_740',
-        'p1_850',
-        'p2_850',
-        'p3_850',
-        'p4_850'
+        'p4_740'
+        // 'p1_850',
+        // 'p2_850',
+        // 'p3_850',
+        // 'p4_850'
       ]}
       rollupLevels={['1s', '2s', '3s']}
       baseChannel={'p1_740'}
--- src/Socket.js
+++ src/Socket.js
@@ -4,14 +4,16 @@ import MultiChannelChart from './Chart';
 import Chart from './Chart';
 import DataChart from './Chart/DataChart';
 
-const mock =  require('./Chart/sensor-example.json')
+// const mock = require('./Chart/sensor-example.json');
 
 export const WebSocketDemo = () => {
   //Public API that will echo messages sent to it back to the client
-  const [socketUrl, setSocketUrl] = useState('ws://192.168.43.61/ws');
+  // const [socketUrl, setSocketUrl] = useState('ws://192.168.43.61/ws');
+  const [socketUrl, setSocketUrl] = useState('ws://192.168.43.243/ws');
   const [messageHistory, setMessageHistory] = useState([]);
   const [counter, setCounter] = useState(0);
   const [stop, setStop] = useState(false);
+  const [running, setRunning] = useState(false);
   const [data, setData] = useState({
     time: [],
     p1_740: [],
@@ -24,33 +26,54 @@ export const WebSocketDemo = () => {
     p4_850: []
   });
 
-  const { sendMessage, lastMessage, readyState } = useWebSocket(socketUrl);
+  const { sendMessage, lastMessage, readyState, getWebSocket } =
+    useWebSocket(socketUrl);
+
+  const save = () => {
+    function download(content, fileName, contentType) {
+      var a = document.createElement('a');
+      var file = new Blob([content], { type: contentType });
+      a.href = URL.createObjectURL(file);
+      a.download = fileName;
+      a.click();
+    }
+    download(JSON.stringify(messageHistory), 'history.json', 'text/plain');
+  };
 
   useEffect(() => {
-    if (stop) return;
+    if (stop) {
+      getWebSocket().close();
+      save();
+      return;
+    }
     if (lastMessage !== null) {
-      setMessageHistory((prev) => prev.concat(lastMessage));
       const d = JSON.parse(lastMessage.data);
+      setMessageHistory((prev) => prev.concat(d));
       //   console.log({ d });
+      const limit = 100;
       if (d.led === 740) {
+        const f = (a) => {
+          return a.slice(-limit);
+        };
+
         setData({
           ...data,
           time: [...data.time, counter],
-          p1_740: [...data.p1_740, d.adc1],
-          p2_740: [...data.p2_740, d.adc2],
-          p3_740: [...data.p3_740, d.adc3],
-          p4_740: [...data.p4_740, d.adc4]
+          p1_740: [...f(data.p1_740), d.adc1],
+          p2_740: [...f(data.p2_740), d.adc2],
+          p3_740: [...f(data.p3_740), d.adc3],
+          p4_740: [...f(data.p4_740), d.adc4]
         });
-        setCounter(counter + 1);
+        if (counter < limit) setCounter(counter + 1);
       } else {
-        setData({
-          ...data,
-          time: [...data.time, counter],
-          p1_850: [...data.p1_850, d.adc1],
-          p2_850: [...data.p2_850, d.adc2],
-          p3_850: [...data.p3_850, d.adc3],
-          p4_850: [...data.p4_850, d.adc4]
-        });
+        // setData({
+        //   ...data,
+        //   time: [...data.time, counter],
+        //   p1_850: [...data.p1_850, d.adc1],
+        //   p2_850: [...data.p2_850, d.adc2],
+        //   p3_850: [...data.p3_850, d.adc3],
+        //   p4_850: [...data.p4_850, d.adc4]
+        // });
       }
     }
   }, [lastMessage, setMessageHistory]);
@@ -72,6 +95,22 @@ export const WebSocketDemo = () => {
   }[readyState];
 
   //   console.log({ data });
+  console.log('render');
+
+  const x = lastMessage ? JSON.parse(lastMessage.data) : {};
+  console.log({ x });
+
+  useEffect(() => {
+    const l = window.addEventListener('keypress', (e) => {
+      console.log(e);
+      if (e.key === ' ') {
+        handleClickSendMessage();
+      }
+      if (e.key === 'Enter') {
+        setStop(true);
+      }
+    });
+  }, []);
 
   return (
     <div>
@@ -88,7 +127,11 @@ export const WebSocketDemo = () => {
       >
         Stop
       </button>
-      {/* {lastMessage ? <div>Last message: {lastMessage.data}</div> : null} */}
+      {lastMessage ? (
+        <pre style={{ fontSize: '40px' }}>
+          {x.adc1} {x.adc2} {x.adc3} {x.adc4}
+        </pre>
+      ) : null}
       <div>
         {data?.time && data.time.length > 1 && <DataChart data={data} />}
       </div>
